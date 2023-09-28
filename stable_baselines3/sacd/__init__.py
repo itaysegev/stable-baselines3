5ef89ca21a7dd8f9363102ca840e1c647aa8c466
Itay Segev <itaysegev11@gmail.com>
2023-09-28 12:00:03 +0300

    misc: Change classes names

--- stable_baselines3/sacd/__init__.py
+++ stable_baselines3/sacd/__init__.py
@@ -1,4 +1,4 @@
-from stable_baselines3.sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from stable_baselines3.sac.sac import SAC
+from stable_baselines3.sacd.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from stable_baselines3.sacd.sacd import SACD
 
-__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "SAC"]
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "SACD"]
--- stable_baselines3/sacd/sacd.py
+++ stable_baselines3/sacd/sacd.py
@@ -11,12 +11,12 @@ from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.policies import BasePolicy, ContinuousCritic
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
 from stable_baselines3.common.utils import get_parameters_by_name, polyak_update
-from stable_baselines3.sac.policies import Actor, CnnPolicy, MlpPolicy, MultiInputPolicy, SACPolicy
+from stable_baselines3.sacd.policies import Actor, CnnPolicy, MlpPolicy, MultiInputPolicy, SACPolicy
 
-SelfSAC = TypeVar("SelfSAC", bound="SAC")
+SelfSAC = TypeVar("SelfSACD", bound="SACD")
 
 
-class SAC(OffPolicyAlgorithm):
+class SACD(OffPolicyAlgorithm):
     """
     Soft Actor-Critic (SAC)
     Off-Policy Maximum Entropy Deep Reinforcement Learning with a Stochastic Actor,
