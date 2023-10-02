1958bd8b11b0a0c67664d383fe7bb53d9e9ec3c7
Itay Segev <itaysegev11@gmail.com>
2023-10-02 12:06:02 +0300

    Dev: Add support for multi-dimensional rewards

--- stable_baselines3/sacd/sacd.py
+++ stable_baselines3/sacd/sacd.py
@@ -251,7 +251,7 @@ class SACD(OffPolicyAlgorithm):
                 next_actions, next_log_prob = self.actor.action_log_prob(replay_data.next_observations)
 
                 # Compute the next Q values: composite and min over all critics targets
-                composite_q_values = tuple(th.mm(t,self.weights_vector.T) for t in
+                composite_q_values = tuple(th.matmul(t,self.weights_vector.T) for t in
                                            self.critic_target(replay_data.next_observations, next_actions))
                 print(composite_q_values[0], "composite_q_values")
                 composite_q_values = th.cat(composite_q_values, dim=1)
